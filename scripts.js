fb3fe44d19f4ef1b498772b6eb1a85ec98dd7058
Kevin Urgilés <kevinurgiles1996@gmail.com>
2019-04-02 21:15:21 -0500

    Cards stored once clicked

--- scripts.js
+++ scripts.js
@@ -1,7 +1,23 @@
 const cards = document.querySelectorAll('.memory-card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard(){
-  this.classList.toggle('flip');
+  this.classList.add('flip');
+
+  if (!hasFlippedCard){
+    //First Click
+    hasFlippedCard = true;
+    firstCard = this;
+  }
+
+  else{
+    //Second Click
+    secondCard = this;
+    cardHasFlipped = false;
+    console.log(firstCard, secondCard);
+  }
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
