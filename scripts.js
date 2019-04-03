a0aced94f49ff17ebc2cbac444165a22ab01c8a8
Kevin Urgilés <kevinurgiles1996@gmail.com>
2019-04-02 21:36:33 -0500

    Solving double-clicking the same card and shuffling cards

--- scripts.js
+++ scripts.js
@@ -5,13 +5,18 @@ let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard(){
+
   if (lockBoard) return;
+  if (this === firstCard) return;
+
   this.classList.add('flip');
 
   if (!hasFlippedCard){
     //First Click
     hasFlippedCard = true;
     firstCard = this;
+
+    return;
   }
 
   else{
@@ -19,15 +24,13 @@ function flipCard(){
     secondCard = this;
     hasFlippedCard = false;
 
-    //Do cards match?
-
     checkForMatch();
 
   }
 }
 
 function checkForMatch(){
-
+  //Do cards match?
   let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
   isMatch ? disableCards() : unflipCards();
 
@@ -36,6 +39,7 @@ function checkForMatch(){
 function disableCards(){
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
+  resetBoard();
 }
 
 function unflipCards(){
@@ -43,8 +47,22 @@ function unflipCards(){
   setTimeout(() => {
     firstCard.classList.remove('flip');
     secondCard.classList.remove('flip');
-    lockBoard = false;
+
+    resetBoard();
   },1000);
 }
+
+function resetBoard(){
+  [hasFlippedCard, lockBoard] =[false, false];
+  [firstCard, secondCard] = [null, null];
+
+}
+
+(function shuffle(){
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+})();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
